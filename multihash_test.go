392e16c89cb1debfb8ac8230b4d38c8008eb7e71
Samuel Li <samli88@users.noreply.github.com>
2018-10-10 09:05:59 -0700

    remove redundant test case type declarations
    Since these are slices of the respective test cases, the inner declaration is
    not needed, as the slice could not contain any other type.

--- multihash_test.go
+++ multihash_test.go
@@ -36,18 +36,18 @@ type TestCase struct {
 }
 
 var testCases = []TestCase{
-	TestCase{"2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", 0x00, "id"},
-	TestCase{"", 0x00, "id"},
-	TestCase{"0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33", 0x11, "sha1"},
-	TestCase{"0beec7b5", 0x11, "sha1"},
-	TestCase{"2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", 0x12, "sha2-256"},
-	TestCase{"2c26b46b", 0x12, "sha2-256"},
-	TestCase{"2c26b46b68ffc68ff99b453c1d30413413", 0xb240, "blake2b-512"},
-	TestCase{"243ddb9e", 0x22, "murmur3"},
-	TestCase{"f00ba4", 0x1b, "keccak-256"},
-	TestCase{"f84e95cb5fbd2038863ab27d3cdeac295ad2d4ab96ad1f4b070c0bf36078ef08", 0x18, "shake-128"},
-	TestCase{"1af97f7818a28edfdfce5ec66dbdc7e871813816d7d585fe1f12475ded5b6502b7723b74e2ee36f2651a10a8eaca72aa9148c3c761aaceac8f6d6cc64381ed39", 0x19, "shake-256"},
-	TestCase{"4bca2b137edc580fe50a88983ef860ebaca36c857b1f492839d6d7392452a63c82cbebc68e3b70a2a1480b4bb5d437a7cba6ecf9d89f9ff3ccd14cd6146ea7e7", 0x14, "sha3-512"},
+	{"2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", 0x00, "id"},
+	{"", 0x00, "id"},
+	{"0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33", 0x11, "sha1"},
+	{"0beec7b5", 0x11, "sha1"},
+	{"2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", 0x12, "sha2-256"},
+	{"2c26b46b", 0x12, "sha2-256"},
+	{"2c26b46b68ffc68ff99b453c1d30413413", 0xb240, "blake2b-512"},
+	{"243ddb9e", 0x22, "murmur3"},
+	{"f00ba4", 0x1b, "keccak-256"},
+	{"f84e95cb5fbd2038863ab27d3cdeac295ad2d4ab96ad1f4b070c0bf36078ef08", 0x18, "shake-128"},
+	{"1af97f7818a28edfdfce5ec66dbdc7e871813816d7d585fe1f12475ded5b6502b7723b74e2ee36f2651a10a8eaca72aa9148c3c761aaceac8f6d6cc64381ed39", 0x19, "shake-256"},
+	{"4bca2b137edc580fe50a88983ef860ebaca36c857b1f492839d6d7392452a63c82cbebc68e3b70a2a1480b4bb5d437a7cba6ecf9d89f9ff3ccd14cd6146ea7e7", 0x14, "sha3-512"},
 }
 
 func (tc TestCase) Multihash() (Multihash, error) {
